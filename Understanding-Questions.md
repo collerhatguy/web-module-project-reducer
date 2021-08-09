7b6664e61e19a01bcf7c068abb1a4e8a246c2c6a
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-08-09 15:31:59 -0400

    completed understanding questions

--- Understanding-Questions.md
+++ Understanding-Questions.md
@@ -1,7 +1,12 @@
 # Understanding Questions:
 1. What are the steps of execution from the pressing of the 1 button to the rendering of our updated value? List what part of the code excutes for each step.
 * The user presses the 1 button.
-* 
-...
+* it triggers the handleAddOne function
+* the dipatch function is called with the addOne function
+* the addOne function returns an object with a type value of add one
+* the addOne function returns an object with a type value of add one
+* the reducer function is called and finds the matching type value 
+* the reducer function returns version fo state where the total has been increased by one 
+* that updates the state and thus the screen
 
 * TotalDisplay shows the total plus 1.
--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useReducer } from 'react';
 import reducer, { initialState } from './reducers';
+import { addOne } from "./actions"
 
 import './App.css';
 
@@ -8,7 +9,7 @@ import CalcButton from './components/CalcButton';
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState)
- 
+  const handleAddOne = () => dispatch(addOne())
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
@@ -19,10 +20,10 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
             
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b> {state.operation}</span>
+              <span id="memory"><b>Memory:</b> {state.memory}</span>
             </div>
             
             <div className="row">
@@ -32,7 +33,7 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
+              <CalcButton value={1} onClick={handleAddOne}/>
               <CalcButton value={2}/>
               <CalcButton value={3}/>
             </div>
